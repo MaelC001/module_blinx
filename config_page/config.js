a3c58f679884202b0f80e84434f8527fad226ce7
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-06-27 16:16:07 -0400

    update config sensor function

--- config_page/config.js
+++ config_page/config.js
@@ -1,4 +1,4 @@
-function _(el){
+function _(el) {
     return document.getElementById(el);
 }
 
@@ -6,40 +6,50 @@ let rootCSS = document.querySelector(':root');
 
 // number of port
 let nmbPort = 3;
-let infoUserSensor = [{}]*nmbPort;
+let infoUserSensor = [{}] * nmbPort;
 // which port have i2c
 let PortsI2C = [1];
 // list of sensor for each different port
 let listAllSensors = {
-    "I2C" : {
-        "temp-sht3x" : {
-            "text" : "temperature (in micro controller)",
-            "addr" : "",
-            "file" : "",
+    "I2C": {
+        "temp-sht3x": {
+            "text": "temperature (in micro controller)",
+            "addr": "",
+            "file": "",
+            'is_input': false,
+            'is_display': false,
         },
-        "humid-sht3x" : {
-            "text" : "humidity (in micro controller)",
-            "addr" : "",
-            "file" : "",
+        "humid-sht3x": {
+            "text": "humidity (in micro controller)",
+            "addr": "",
+            "file": "",
+            'is_input': false,
+            'is_display': false,
         },
     },
-    "DigiAnalog" : {
-        "" : {
-            "text" : "",
-            "file" : "",
+    "DigiAnalog": {
+        "": {
+            "text": "",
+            "file": "",
+            'is_input': false,
+            'is_display': false,
         },
-        "led pin2" : {
-            "text" : "led (in micro controller) (pin 2)",
-            "file" : "",
+        "led pin2": {
+            "text": "led (in micro controller) (pin 2)",
+            "file": "",
+            'is_input': false,
+            'is_display': false,
         },
-        "custom" : {
-            "text" : "custom",
-            "file" : "",
+        "custom": {
+            "text": "custom",
+            "file": "",
+            'is_input': false,
+            'is_display': false,
         }
     },
 };
 let listSensors = {};
-let listPortAfficher = [0,0,0];
+let listPortAfficher = [0, 0, 0];
 
 textPopup = "\
 <div class='form-popup' id='myForm$IdPopupReplacement$'>\
@@ -62,11 +72,11 @@ textPopupName = "\
 idPopup = [1, 2, 3];
 
 
-function loadSelectPort(){
+function loadSelectPort() {
     create_dic();
 
     // load the different element in the html page
-    let htmlItem =  _("selectorPorts");
+    let htmlItem = _("selectorPorts");
     let textHTML = '';
     // the information of the sensor on the micro controller
     textHTML += "<div class='grid-item itemOnMicro'>";
@@ -79,29 +89,29 @@ function loadSelectPort(){
     // the image
     textHTML += "<div class='grid-item itemImage'><img src='./image.png'></div>";
     for (let i = 0; i < nmbPort; i++) {
-        let y = i+1;
-        let nameResultID = "sensorsPort"+y;
-        if(PortsI2C.includes(i)){
+        let y = i + 1;
+        let nameResultID = "sensorsPort" + y;
+        if (PortsI2C.includes(i)) {
             // the sensor selector, for each port
-            textHTML += "<div class='grid-item itemPortSensor"+y+"' id='div_"+nameResultID+"'>";
+            textHTML += "<div class='grid-item itemPortSensor" + y + "' id='div_" + nameResultID + "'>";
             textHTML += " <p>I2C Port</p>"
-            textHTML += " <select class='singleSensorI2C' id='"+nameResultID+"'";
-            textHTML += "     name='sensorsPort' onchange='changeSensor(\""+y+"\")'></select>";
-            textHTML += " <button type='button' class='btn' onclick='verify_i2c("+y+")'>Scan I2C</button>";
-            textHTML += " <button type='button' onclick='openForm("+y+")'> Config the sensor </button>";
+            textHTML += " <select class='singleSensorI2C' id='" + nameResultID + "'";
+            textHTML += "     name='sensorsPort' onchange='changeSensor(\"" + y + "\")'></select>";
+            textHTML += " <button type='button' class='btn' onclick='verify_i2c(" + y + ")'>Scan I2C</button>";
+            textHTML += " <button type='button' onclick='openForm(" + y + ")'> Config the sensor </button>";
             textHTML += "</div>";
-        } else{
+        } else {
             // the sensor selector, for each port
-            textHTML += "<div class='grid-item itemPortSensor"+y+"' id='div_"+nameResultID+"'>";
-            textHTML += " <select class='singleSensor' id='"+nameResultID+"'";
-            textHTML += "     name='sensorsPort' onchange='changeSensor(\""+y+"\")'></select>";
-            textHTML += " <button type='button' onclick='openForm("+y+")'> Config the sensor </button>";
+            textHTML += "<div class='grid-item itemPortSensor" + y + "' id='div_" + nameResultID + "'>";
+            textHTML += " <select class='singleSensor' id='" + nameResultID + "'";
+            textHTML += "     name='sensorsPort' onchange='changeSensor(\"" + y + "\")'></select>";
+            textHTML += " <button type='button' onclick='openForm(" + y + ")'> Config the sensor </button>";
             textHTML += "</div>";
         }
     }
 
     textHTML += '<div id="listPopup">';
-    idPopup.forEach( id => {
+    idPopup.forEach(id => {
         textHTML += textPopup.replaceAll("$IdPopupReplacement$", id)
     });
     textHTML += '</div>';
@@ -132,16 +142,16 @@ function loadSelectPort(){
     });
 }
 
-function changeSensor(port){
-    value = _('sensorsPort'+port).value;
+function changeSensor(port) {
+    value = _('sensorsPort' + port).value;
     let tempo = {
-        "name" : value,
-        "userName" : "",
-        "port" : port,
-        "borne inferieure" : -1,
-        "borne supeieure" : -1,
+        "name": value,
+        "userName": "",
+        "port": port,
+        "borne inferieure": -1,
+        "borne supeieure": -1,
     };
-    infoUserSensor[port-1] = tempo;
+    infoUserSensor[port - 1] = tempo;
 }
 
 function format(option) {
@@ -149,68 +159,67 @@ function format(option) {
     if (!option.id || option.id == 'custom') {
         return option.text;
     }
-    var ob = '<span class="spanSelector">' + option.text + '</span><br><img class="imgSelector" src="https://lh4.ggpht.com/wKrDLLmmxjfRG2-E-k5L5BUuHWpCOe4lWRF7oVs1Gzdn5e5yvr8fj-ORTlBF43U47yI=w64"/>';	// replace image source with option.img (available in JSON)
+    var ob = '<span class="spanSelector">' + option.text + '</span><br><img class="imgSelector" src="https://lh4.ggpht.com/wKrDLLmmxjfRG2-E-k5L5BUuHWpCOe4lWRF7oVs1Gzdn5e5yvr8fj-ORTlBF43U47yI=w64"/>'; // replace image source with option.img (available in JSON)
     return ob;
 };
 
 function openForm(y) {
-    let types = $('#sensorsPort'+y).val();
-    document.getElementById("myForm"+y).style.display = "block";
-    if (typeof(types) == "string"){
-        if(types == ""){
+    let types = $('#sensorsPort' + y).val();
+    document.getElementById("myForm" + y).style.display = "block";
+    if (typeof (types) == "string") {
+        if (types == "") {
             closeForm(y);
-        }
-        else{
+        } else {
             let text = textPopupName.replaceAll("$IdPopupReplacement$", y);
-            document.getElementById("myFormSensor"+y).innerHTML = text.replaceAll("$TypePopupReplacement$", types);
+            document.getElementById("myFormSensor" + y).innerHTML = text.replaceAll("$TypePopupReplacement$", types);
         }
-    } else if (typeof(types) == "object"){
-        if(types.length <= 0){
+    } else if (typeof (types) == "object") {
+        if (types.length <= 0) {
             closeForm(y);
-        }
-        else{
+        } else {
             let text = '';
             let number = 0;
             types.forEach(type => {
-                let tempo = textPopupName.replaceAll("$IdPopupReplacement$", ''+y+number);
+                let tempo = textPopupName.replaceAll("$IdPopupReplacement$", '' + y + number);
                 text += tempo.replaceAll("$TypePopupReplacement$", type);
                 number += 1;
             });
-            document.getElementById("myFormSensor"+y).innerHTML = text;
+            document.getElementById("myFormSensor" + y).innerHTML = text;
         }
     }
 }
 
 function closeForm(y) {
-    document.getElementById("myForm"+y).style.display = "none";
+    document.getElementById("myForm" + y).style.display = "none";
 }
 
-function saveConfigPort(port){}
-function saveConfig(){
+function saveConfigPort(port) {}
+
+function saveConfig() {
     console.log(infoUserSensor);
 }
 
-function verify_i2c(port){
+function verify_i2c(port) {
     let cmd = "scan_i2c";
     write(cmd, idCmd = 0);
     return read().then(e => verify_json(e, verify));
 
-    function verify(json){
+    function verify(json) {
         let error = []
-        let values = _('sensorsPort'+port).value;
+        let values = _('sensorsPort' + port).value;
         values.forEach(test);
         return error;
 
-        function test(name){
-            if (!json['result'].includes(listAllSensors['I2C'][name]['addr'])){
+        function test(name) {
+            if (!json['result'].includes(listAllSensors['I2C'][name]['addr'])) {
                 error.push(name);
             }
         }
     }
 }
 
-function config_sensor(){
-    if (!verify_i2c()){
+function config_sensor() {
+    if (!verify_i2c()) {
         alert('i2c not correct');
     }
     var json_config = [];
@@ -220,34 +229,50 @@ function config_sensor(){
     var port2 = _('sensorsPort2').value;
 
     port.forEach(DigiAnalogFunction);
-    function DigiAnalogFunction(name){
-        if(name != "" && name != "costum"){
+    let i = 0;
+    function DigiAnalogFunction(name) {
+        if (name != "" && name != "costum") {
             json_push(name, 'DigiAnalog');
+            let info = infoUserSensor[i];
+            json_sensor[info['name']] = {
+                'new_name': info['userName'],
+                'is_input': listAllSensors['DigiAnalog'][name]['is_input'],
+                'is_display': listAllSensors['DigiAnalog'][name]['is_display'],
+                'channels': info['channels'],
+            }
+            i = 2;
         }
     }
 
     port2.forEach(I2CFunction);
-    function I2CFunction(name){
+    function I2CFunction(name) {
         json_push(name, 'I2C');
+        let info = infoUserSensor[1];
+        json_sensor[info['name']] = {
+            'new_name': info['userName'],
+            'is_input': listAllSensors['I2C'][name]['is_input'],
+            'is_display': listAllSensors['I2C'][name]['is_display'],
+            'channels': info['channels'],
+        }
     }
 
     config_sensor_serial(json_config, json_sensor);
 
-    function json_push(name, type){
+    function json_push(name, type) {
         json_config.push({
-            'name' : name,
-            'file' : listAllSensors[type][name]['file'],
+            'name': name,
+            'file': listAllSensors[type][name]['file'],
         });
     }
 }
 
-function create_dic(){
-    for(var key1 in listAllSensors) {
+function create_dic() {
+    for (var key1 in listAllSensors) {
         listSensors[key1] = []
-        for(var key2 in listAllSensors[key1]) {
+        for (var key2 in listAllSensors[key1]) {
             listSensors[key1].push({
-                'id' : key2,
-                'text' : listAllSensors[key1][key2]['text'],
+                'id': key2,
+                'text': listAllSensors[key1][key2]['text'],
             })
         }
     }
