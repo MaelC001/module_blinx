4c9c19ab768fbfa558a316d34efece3d71571192
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-07-18 14:14:14 -0400

    update liste

--- config_page/index.js
+++ config_page/index.js
@@ -64,7 +64,7 @@ let listAllSensors = {
     "DigiAnalog": {
         "buzzer_digi": {
             "text": "Digital Buzzer",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/7/7d/KS0349_5-1.png',
@@ -78,7 +78,7 @@ let listAllSensors = {
         },
         "buzzer_passive": {
             "text": "Passive Buzzer",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/a/af/KS0019_%284%29.jpg',
@@ -92,7 +92,7 @@ let listAllSensors = {
         },
         "relay_5v": {
             "text": "5V Relay",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/b/b0/Ks0011.png',
@@ -120,7 +120,7 @@ let listAllSensors = {
         },
         "3w_led": {
             "text": "3W LED",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/7/75/KS0010_%284%29.jpg',
@@ -134,7 +134,7 @@ let listAllSensors = {
         },
         "white_led": {
             "text": "White LED",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/3/38/Ks0016-1.png',
@@ -148,7 +148,7 @@ let listAllSensors = {
         },
         "red_led": {
             "text": "Red LED",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/4/4d/Ks0232-1.png',
@@ -162,7 +162,7 @@ let listAllSensors = {
         },
         "green_led": {
             "text": "Green LED",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/d/da/Ks0233-1.png',
@@ -176,7 +176,7 @@ let listAllSensors = {
         },
         "yellow_led": {
             "text": "Yellow LED",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/b/b2/Ks0234-1.png',
@@ -190,7 +190,7 @@ let listAllSensors = {
         },
         "blue_led": {
             "text": "Blue LED",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'https://wiki.keyestudio.com/images/f/ff/Ks0235-1.png',
@@ -202,11 +202,67 @@ let listAllSensors = {
                 'pin': 0,
             }],
         },
+        "SFE_Reed_Switch": {
+            "text": "Hall Magnetic Sensor",
+            "file": "digital",
+            'is_input': false,
+            'is_display': false,
+            'urlImage': 'https://wiki.keyestudio.com/images/d/db/KS0020_%282%29.jpg',
+            'url': '',
+            'numberPin': 1,
+            'channels': [{
+                'type': 'Digital',
+                'id': 0,
+                'pin': 0,
+            }],
+        },
+        "Reed_Switch": {
+            "text": "Reed Switch",
+            "file": "digital",
+            'is_input': true,
+            'is_display': false,
+            'urlImage': 'https://wiki.keyestudio.com/images/8/86/Ks0038.png',
+            'url': '',
+            'numberPin': 1,
+            'channels': [{
+                'type': 'Digital',
+                'id': 0,
+                'pin': 0,
+            }],
+        },
+        "AD_KEY_Button": {
+            "text": "AD KEY Button Module",
+            "file": "analog",
+            'is_input': true,
+            'is_display': false,
+            'urlImage': 'https://wiki.keyestudio.com/images/e/ee/Ks0466-1.png',
+            'url': '',
+            'numberPin': 1,
+            'channels': [{
+                'type': 'Analog',
+                'id': 0,
+                'pin': 0,
+            }],
+        },
+        "knock_sensor": {
+            "text": "Knock Sensor",
+            "file": "analog",
+            'is_input': true,
+            'is_display': false,
+            'urlImage': 'https://wiki.keyestudio.com/images/c/c9/Ks0024.png',
+            'url': '',
+            'numberPin': 1,
+            'channels': [{
+                'type': 'analog',
+                'id': 0,
+                'pin': 0,
+            }],
+        },
     },
     "In": {
         "led": {
             "text": "led (in micro controller) (pin 2)",
-            "file": "digital_ou",
+            "file": "digital",
             'is_input': false,
             'is_display': false,
             'urlImage': 'http://icons.iconarchive.com/icons/icons8/ios7/512/Users-User-Male-icon.png',
