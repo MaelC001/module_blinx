5a39fbd24f7549d5c47407eba32ecc7a1976b9e0
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-08-01 08:18:20 +0200

    change boot.py

--- file_modify/ports/esp8266/modules/inisetup.py
+++ file_modify/ports/esp8266/modules/inisetup.py
@@ -48,35 +48,20 @@ def setup():
     vfs = uos.VfsLfs2(bdev)
     uos.mount(vfs, "/")
     with open("boot.py", "w") as f:
-        f.write(
-            """\
-# This file is executed on every boot (including wake-boot from deepsleep)
+        f.write("""\
 #import esp
 #esp.osdebug(None)
-import uos, machine
-#uos.dupterm(None, 1) # disable REPL on UART(0)
-import gc
-gc.collect()
-#import wifi_config, config_file
-#wlan = wifi_config.get_connection()
-#if wlan is None:
-#    print("Could not initialize the network connection.")
-#else :
-#    config_file.setIp(wlan.ifconfig())
-#password, ports = config_file.infoWebrepl()
-#ap_ssid, ap_password, ap_authmode, id, port = config_file.infoWifi()
-#import webrepl
-#webrepl.start(port=port, password=password, ssid_wifi=ap_ssid, password_wifi=ap_password, authmode_wifi=ap_authmode, html=True, html_text=None)
-#webrepl.start(port=ports, password=password)
 from machine import Pin,I2C
-import ssd1306
+import gc, ssd1306, webrepl #, repl
+gc.collect()
+#webrepl.change_rpc(repl.read_input)
+#webrepl.start()
 p2 = Pin(2, Pin.OUT)
 p2.value(0)
 i2c=I2C(sda=Pin(4),scl=Pin(5))
 display=ssd1306.SSD1306_I2C(128,32,i2c)
 display.text('MicroPython',0,0,1)
-display.text('Connected',11,11,1)
 display.show()
-"""
-        )
+#repl.launch()
+""")
     return vfs
