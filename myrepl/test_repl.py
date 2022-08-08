ffe7423313d804cb72df798e99d0dc960e8f429b
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-08-08 09:32:34 -0400

    update test to unittest (only for rpc now)

--- myrepl/test_repl.py
+++ myrepl/test_repl.py
@@ -1,5 +1,5 @@
 import time, os
-
+import unittest
 # https://raw.githubusercontent.com/MaelC001/sensor_blinx/main/
 # 
 # def function(value, error):
@@ -15,106 +15,98 @@ except Exception:
 array_file = os.listdir()
 __register = repl.__register
 
-# test function verification
-assert repl.verification('a', str)
-assert repl.verification('a', int) == TypeError
-assert repl.verification(0, str) == TypeError
-assert repl.verification(0, int)
-assert repl.verification('a', str, ['a'])
-assert repl.verification('a', str, ['b']) == TypeError
-assert repl.verification(0, int, [0])
-assert repl.verification(0, int, [1]) == TypeError
-assert repl.verification('a', str, ['b'], False)
-assert repl.verification('a', str, ['a'], False) == TypeError
-assert repl.verification(0, int, [1], False)
-assert repl.verification(0, int, [0], False) == TypeError
+class TestStringMethods(unittest.TestCase):
+    def test_verification(self):
+        # test function verification
+        self.assertTrue(repl.verification('a', str))
+        self.assertTrue(repl.verification(0, int))
+        with self.assertRaises(TypeError):
+            repl.verification('a', int)
+        with self.assertRaises(TypeError):
+            repl.verification(0, str)
 
-# test the register
-@repl.__register('test1', False)
-def test1(a):
-    return a
-@repl.__register('test2', False)
-def test2(a):
-    raise TypeError(a)
+        self.assertTrue(repl.verification('a', str, ['a']))
+        with self.assertRaises(TypeError):
+            repl.verification('a', str, ['b'])
 
-@repl.__register('test3', True)
-def test3(a):
-    return test1(a)
-@repl.__register('test4', True)
-def test4(a):
-    return test2(a)
+        self.assertTrue(repl.verification('a', str, ['b'], False))
+        with self.assertRaises(TypeError):
+            repl.verification('a', str, ['a'], False)
 
-assert 'test1' in repl.__register
-assert 'test2' in repl.__register
-assert 'test3' in repl.__register
-assert 'test4' in repl.__register
-assert repl.__register['test1']('a', id = 0) == {"result":'a', 'id': 0}
-assert repl.__register['test2']('a', id = 0) == {"error":{"code": -32000, "message": 'error'}, 'id': 0}
-assert repl.__register['test3']('a', id = 0) == {"result":'a', 'id': 0}
-assert repl.__register['test4']('a', id = 0) == {"error":{"code": -32000, "message": 'error'}, 'id': 0}
+        self.assertTrue(repl.verification(0, int, [0]))
+        with self.assertRaises(TypeError):
+            repl.verification(0, int, [1])
 
-repl.__register = __register
+        self.assertTrue(repl.verification(0, int, [1], False))
+        with self.assertRaises(TypeError):
+            repl.verification(0, int, [0], False)
 
+    def test_register(self):
+        # test the register
+        @repl.__register('test1', False)
+        def test1(a):
+            return a
+        @repl.__register('test2', False)
+        def test2(a):
+            raise TypeError(a)
 
-# test read_input function
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : 1}, how_send = 'return') == {'result': array_file, 'id': 1}
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'create', 'id' : None}, how_send = 'return') == {'result': '', 'id': None}
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'create', 'id' : 0}, how_send = 'return') == {'id': 0, 'error': {'code': -32000, 'message': 'a.txt don\'t have a correct value'}}
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : 0}, how_send = 'return') == {'result': ['a.txt'] + array_file, 'id': 0}
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'read', 'id' : 1}, how_send = 'return') == {'result': '', 'id': 1}
+        @repl.__register('test3', True)
+        def test3(a):
+            return test1(a)
+        @repl.__register('test4', True)
+        def test4(a):
+            return test2(a)
 
-assert repl.read_input({'params': {'name': 'a.txt', 'text': 'bonjour'}, 'method': 'write', 'id' : 1}, how_send = 'return') == {'result': '', 'id': 1}
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : 1}, how_send = 'return') == {'result': ['a.txt'] + array_file, 'id': 1}
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'read', 'id' : 1}, how_send = 'return') == {'result': 'bonjour', 'id': 1}
+        self.assertTrue('test1' in repl.__register)
+        self.assertTrue('test2' in repl.__register)
+        self.assertTrue('test3' in repl.__register)
+        self.assertTrue('test4' in repl.__register)
 
-assert repl.read_input({'params': {'name': 'a.txt', 'text': ' m', 'format': 'a'}, 'method': 'write', 'id' : 1}, how_send = 'return') == {'result': '', 'id': 1}
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : 1}, how_send = 'return') == {'result': ['a.txt'] + array_file, 'id': 1}
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'read', 'id' : 1}, how_send = 'return') == {'result': 'bonjour m', 'id': 1}
+        self.assertEqual(repl.__register['test1']('a', id = 0), {"result":'a', 'id': 0})
+        self.assertEqual(repl.__register['test2']('a', id = 0), {"error":{"code": -32000, "message": 'error'}, 'id': 0})
+        self.assertEqual(repl.__register['test3']('a', id = 0), {"result":'a', 'id': 0})
+        self.assertEqual(repl.__register['test4']('a', id = 0), {"error":{"code": -32000, "message": 'error'}, 'id': 0})
 
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'remove', 'id' : 1}, how_send = 'return') == {'result': '', 'id': 1}
-assert repl.read_input({'params': {'name': 'a.txt'}, 'method': 'remove', 'id' : 1}, how_send = 'return') == {'id': 1, 'error': {'code': -32000, 'message': 'a.txt don\'t have a correct value'}}
-assert repl.read_input({'params': {'name': 10}, 'method': 'read', 'id' : 1}, how_send = 'return') == {'id': 1, 'error': {'code': -32000, 'message': 'the type of 10 isn\'t < class \'str\' > '}}
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : 1}, how_send = 'return') == {'result': array_file, 'id': 1}
+        repl.__register = __register
 
-assert repl.read_input({'params': {'value': 0}, 'method': 'led', 'id' : 1}, how_send = 'return') == {'result': '', 'id': 1}
-assert repl.read_input({'params': {'value': 1}, 'method': 'led', 'id' : 1}, how_send = 'return') == {'result': '', 'id': 1}
-assert repl.read_input({'params': {'value': 2}, 'method': 'led', 'id' : 1}, how_send = 'return') == {'id': 1, 'error': {'code': -32000, 'message': '2 don\'t have a correct value'}}
+    def test_file_func(self):
+        # test function rpc :
+        self.assertEqual(repl.list_file(), array_file)
+        self.assertEqual(repl.create_file(name = 'a.txt'), '')
+        with self.assertRaises(FileExistsError):
+            repl.create_file(name = 'a.txt')
+        self.assertEqual(repl.list_file(), ['a.txt'] + array_file)
+        self.assertEqual(repl.read_file(name = 'a.txt'), '')
+        self.assertEqual(repl.write_file(name = 'a.txt', text = 'bonjour'), '')
+        self.assertEqual(repl.read_file(name = 'a.txt'), 'bonjour')
+        self.assertEqual(repl.write_file(name = 'a.txt', text = ' m', format = 'a'), '')
+        self.assertEqual(repl.read_file(name = 'a.txt'), 'bonjour m')
+        self.assertEqual(repl.write_file(name = 'a.txt', text = ' m', format = 'w'), '')
+        self.assertEqual(repl.remove_file(name = 'a.txt'), '')
+        with self.assertRaises(FileNotFoundError):
+            repl.create_file(name = 'a.txt')
+        with self.assertRaises(FileNotFoundError):
+            repl.read_file(name = 'a.txt')
+        self.assertEqual(repl.list_file(), array_file)
 
+    def test_ead_input(self):
+        # test read_input function
+        self.assertEqual(repl.read_input({'method':'liste', 'params':{}, 'id' : 1}, how_send = 'return'), {'result': array_file, 'id': 1})
+        self.assertEqual(repl.read_input({'method':'liste', 'params':[], 'id' : '1'}, how_send = 'return'), {'result': array_file, 'id': '1'})
+        self.assertEqual(repl.read_input({'method':'liste', 'params':{}, 'id' : None}, how_send = 'return'), {'result': array_file, 'id': None})
 
-# test function rpc :
-assert repl.list_file() == array_file
-assert repl.create_file(name = 'a.txt') == ''
-assert repl.create_file(name = 'a.txt') == Exception
-assert repl.list_file() == ['a.txt'] + array_file
-assert repl.read_file(name = 'a.txt') == ''
-assert repl.write_file(name = 'a.txt', text = 'bonjour') == ''
-assert repl.read_file(name = 'a.txt') == 'bonjour'
-assert repl.write_file(name = 'a.txt', text = ' m', format = 'a') == ''
-assert repl.read_file(name = 'a.txt') == 'bonjour m'
-assert repl.write_file(name = 'a.txt', text = ' m', format = 'w') == ''
-assert repl.remove_file(name = 'a.txt') == ''
-assert repl.remove_file(name = 'a.txt') == Exception
-assert repl.write_file(name = 'a.txt', text = 'bonjour') == Exception
-assert repl.read_file(name = 'a.txt') == Exception
-assert repl.list_file() == array_file
+        self.assertEqual(repl.read_input({'method':'liste', 'params':{}}, how_send = 'return'), {'result': {"code": -32600, "message": "Invalid Request"}, 'id': None})
+        self.assertEqual(repl.read_input({'method':'liste', 'params':{}, 'id' : [1]}, how_send = 'return'), {'result': {"code": -32600, "message": "Invalid Request"}, 'id': None})
+        self.assertEqual(repl.read_input({'method':0, 'params':{}, 'id' : 1}, how_send = 'return'), {'result': {"code": -32600, "message": "Invalid Request"}, 'id': None})
 
+        self.assertEqual(repl.read_input({'method':'aaasssafddsgdf', 'params':{}, 'id' : 1}, how_send = 'return'), {'result': {"code": -32601, "message": "Method not found"}, 'id': None})
+        self.assertEqual(repl.read_input({'method':'liste', 'params':'sdfds', 'id' : 1}, how_send = 'return'), {'result': {"code": -32602, "message": "Invalid params"}, 'id': None})
+        self.assertEqual(repl.read_input({'method':'liste', 'params':12.34, 'id' : 1}, how_send = 'return'), {'result': {"code": -32602, "message": "Invalid params"}, 'id': None})
 
-# test read_input function
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : 1}, how_send = 'return') == {'result': array_file, 'id': 1}
-assert repl.read_input({'method':'liste', 'params':[], 'id' : '1'}, how_send = 'return') == {'result': array_file, 'id': '1'}
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : None}, how_send = 'return') == {'result': array_file, 'id': None}
-
-assert repl.read_input({'method':'liste', 'params':{}}, how_send = 'return') == {'result': {"code": -32600, "message": "Invalid Request"}, 'id': None}
-assert repl.read_input({'method':'liste', 'params':{}, 'id' : [1]}, how_send = 'return') == {'result': {"code": -32600, "message": "Invalid Request"}, 'id': None}
-assert repl.read_input({'method':0, 'params':{}, 'id' : 1}, how_send = 'return') == {'result': {"code": -32600, "message": "Invalid Request"}, 'id': None}
-
-assert repl.read_input({'method':'aaasssafddsgdf', 'params':{}, 'id' : 1}, how_send = 'return') == {'result': {"code": -32601, "message": "Method not found"}, 'id': None}
-assert repl.read_input({'method':'liste', 'params':'sdfds', 'id' : 1}, how_send = 'return') == {'result': {"code": -32602, "message": "Invalid params"}, 'id': None}
-assert repl.read_input({'method':'liste', 'params':12.34, 'id' : 1}, how_send = 'return') == {'result': {"code": -32602, "message": "Invalid params"}, 'id': None}
-
-# test decode_input function
-assert repl.decode_input(b"{'method':'liste', 'params':{}, 'id' : 1}\n", how_send = 'return') == {'result': array_file, 'id': 1}
-assert repl.decode_input(b"{'method':'liste', 'params':{}, 'id' 1}\n", how_send = 'return') == {'error': {"code": -32700, "message": "Parse error"}, 'id': 1}
+    def test_file_func(self):
+        # test decode_input function
+        self.assertEqual(repl.decode_input(b"{'method':'liste', 'params':{}, 'id' : 1}\n", how_send = 'return'), {'result': array_file, 'id': 1})
+        self.assertEqual(repl.decode_input(b"{'method':'liste', 'params':{}, 'id' 1}\n", how_send = 'return'), {'error': {"code": -32700, "message": "Parse error"}, 'id': 1})
 
 
 
@@ -150,3 +142,9 @@ assert repl.decode_input(b"{'method':'liste', 'params':{}, 'id' 1}\n", how_send 
     # verification_list_sensor
 
     # ota_update
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
