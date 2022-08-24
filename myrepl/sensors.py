85305c1dc19eb2e775794567a3fd01212a6feeb3
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-08-24 14:26:35 -0400

    remove the crc

--- myrepl/sensors.py
+++ myrepl/sensors.py
@@ -18,20 +18,6 @@ def register(fn, name, etape, waiting = 0, args = {}):
     return fn
   return wrapper(fn)
 
-# https://github.com/ralf1070/Adafruit_Python_SHT31/
-def crc8_sht3(buffer):
-	""" Polynomial 0x31 (x8 + x5 +x4 +1) """
-	polynomial = 0x31
-	crc = 0xFF
-	index = 0
-	for index in range(0, len(buffer)):
-		crc ^= buffer[index]
-		for i in range(8, 0, -1):
-			if crc & 0x80:
-				crc = (crc << 1) ^ polynomial
-			else:
-				crc = (crc << 1)
-	return crc & 0xFF
 
 
 def get_all_function_sensor():
